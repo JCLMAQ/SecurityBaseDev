8fbcd9cd75ccb24cbd9f81871656451a9cf71ecf
JCM <jcl.maquinay@gmail.com>
2018-06-30 11:06:09 +0200

    Route Guard test

--- web/src/app/shared/guard.service.ts
+++ web/src/app/shared/guard.service.ts
@@ -8,46 +8,18 @@ import { map, switchMap } from 'rxjs/operators';
 // prevent non connected users to access the app
 @Injectable()
 export class IsUserTodosGuard implements CanActivate {
-
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router
   ) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    // return this.authenticationService.currentUser.pipe(
-    //   switchMap((u) => {
-
-    //       .then();
-    //   }),
-    // );
     return this.authenticationService.hasRole('userTodos');
-    // return await this.authenticationService.checkCredentials().then(result => {
-    //   if(result) {
-    //     if(state.url === '/login' || state.url === '/register') {
-    //       this.router.navigate(['/mainView']);
-    //     } else {
-    //       return true;
-    //     }
-    //   } else {
-    //     this.router.navigate(['/login']);
-    //   }
-    // }).catch((errorMessage) => {
-    //   let url = state.url;
-    //   if(url === '/login') {
-    //     return true;
-    //   } else if(url === '/register') {
-    //     return true;
-    //   } else {
-    //     this.router.navigate(['/login']);
-    //   }
-    // });
   }
 }
 
 @Injectable()
 export class IsAdminGuard implements CanActivate {
-
   constructor(
     private authenticationService: AuthenticationService,
     private router: Router

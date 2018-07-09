4df9cb0167603379a844889713915d1f64022d0f
JCM <jcl.maquinay@gmail.com>
2018-07-09 19:20:49 +0200

    revenir sans blocage Wakanda db

--- backend/models/main/model.js
+++ backend/models/main/model.js
@@ -1,7 +1,1 @@
-
-include('./methods/User/User-methods.js');
-include('./methods/Todo/Todo-methods.js');
-include('./methods/UserTodoAssign/UserTodoAssign-methods.js');
-include('./methods/Todo/Todo-events.js');
-include('./methods/User/User-events.js');
-include('./methods/ChangeTracking/ChangeTracking-methods.js');
+include('./methods/User/User-methods.js');include('./methods/Todo/Todo-methods.js');include('./methods/UserTodoAssign/UserTodoAssign-methods.js');include('./methods/Todo/Todo-events.js');include('./methods/User/User-events.js');include('./methods/ChangeTracking/ChangeTracking-methods.js');

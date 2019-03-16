df76c9642f140099c0dce2b752c51751044f4c4a
JCM <jcl.maquinay@gmail.com>
2019-03-16 19:34:21 +0100

    Caroussel modif

--- web/src/app/shared/carousel/carousel.component.ts
+++ web/src/app/shared/carousel/carousel.component.ts
@@ -10,9 +10,8 @@ import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
   providers: [NgbCarouselConfig]
 })
 export class CarouselComponent  {
- // export class CarouselComponent implements OnInit {
- // images: Array<string>;
-  images = [1, 2, 3, 4].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
+
+  images = [1, 2, 3, 4, 5].map(() => `https://picsum.photos/900/500?random&t=${Math.random()}`);
 
   constructor(private _http: HttpClient, config: NgbCarouselConfig) {
     // customize default values of carousels used by this component tree
@@ -21,18 +20,5 @@ export class CarouselComponent  {
     config.keyboard = false;
     config.pauseOnHover = false;
   }
-
-  // ngOnInit() {
-  //   this._http.get('https://picsum.photos/list')
-  //       .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
-  //       .subscribe(images => this.images = images);
-  // }
-
-  // private _randomImageUrls(images: Array<{id: number}>): Array<string> {
-  //   return [1, 2, 3].map(() => {
-  //     const randomId = images[Math.floor(Math.random() * images.length)].id;
-  //     return `https://picsum.photos/900/500?image=${randomId}`;
-  //   });
-  //}
 
 }

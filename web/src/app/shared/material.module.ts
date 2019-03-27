99e4a9bfc1fd92227b8c7c603ed884d29f7385aa
JCM <jcl.maquinay@gmail.com>
2019-03-27 17:08:36 +0100

    Ng update core

--- web/src/app/shared/material.module.ts
+++ web/src/app/shared/material.module.ts
@@ -1,74 +1,31 @@
 import { NgModule } from '@angular/core';
 import {
   	MatMenuModule,
-	MatSidenavModule, 
-		  MatToolbarModule, 
-		   	  
+	MatSidenavModule,
+		  MatToolbarModule,
+
 		  MatListModule,
 		  MatGridListModule,
 		  MatCardModule,
 		  MatStepperModule,
 		  MatTabsModule,
 		  MatExpansionModule,
-		  
+
 		  MatButtonModule,
-		  MatButtonToggleModule, 
+		  MatButtonToggleModule,
 		  MatChipsModule,
 		  MatIconModule,
 		  MatProgressSpinnerModule,
 		  MatProgressBarModule,
-		  
+
 		  MatDialogModule,
 		  MatTooltipModule,
 		  MatSnackBarModule,
-		  
+
 		  MatTableModule,
 		  MatSortModule,
 		  MatPaginatorModule,
-		  
-		  MatAutocompleteModule,
-		  MatCheckboxModule,
-		  MatDatepickerModule,
-		  MatFormFieldModule,
-		  MatInputModule,
-		  MatRadioModule,
-		  MatSelectModule,
-		  MatSliderModule,
-		  MatSlideToggleModule, 
-		  
 
-} from '@angular/material';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-
-const MODULES = [
-	MatMenuModule,
-  MatSidenavModule, 
-		  MatToolbarModule, 
-		   	  
-		  MatListModule,
-		  MatGridListModule,
-		  MatCardModule,
-		  MatStepperModule,
-		  MatTabsModule,
-		  MatExpansionModule,
-		  
-		  MatButtonModule,
-		  MatButtonToggleModule, 
-		  MatChipsModule,
-		  MatIconModule,
-		  MatProgressSpinnerModule,
-		  MatProgressBarModule,
-		  
-		  MatDialogModule,
-		  MatTooltipModule,
-		  MatSnackBarModule,
-		  
-		  MatTableModule,
-		  MatSortModule,
-		  MatPaginatorModule,
-		  
 		  MatAutocompleteModule,
 		  MatCheckboxModule,
 		  MatDatepickerModule,
@@ -78,7 +35,50 @@ const MODULES = [
 		  MatSelectModule,
 		  MatSliderModule,
 		  MatSlideToggleModule,
-		
+
+
+} from '@angular/material';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const MODULES = [
+	MatMenuModule,
+  MatSidenavModule,
+		  MatToolbarModule,
+
+		  MatListModule,
+		  MatGridListModule,
+		  MatCardModule,
+		  MatStepperModule,
+		  MatTabsModule,
+		  MatExpansionModule,
+
+		  MatButtonModule,
+		  MatButtonToggleModule,
+		  MatChipsModule,
+		  MatIconModule,
+		  MatProgressSpinnerModule,
+		  MatProgressBarModule,
+
+		  MatDialogModule,
+		  MatTooltipModule,
+		  MatSnackBarModule,
+
+		  MatTableModule,
+		  MatSortModule,
+		  MatPaginatorModule,
+
+		  MatAutocompleteModule,
+		  MatCheckboxModule,
+		  MatDatepickerModule,
+		  MatFormFieldModule,
+		  MatInputModule,
+		  MatRadioModule,
+		  MatSelectModule,
+		  MatSliderModule,
+		  MatSlideToggleModule,
+      BrowserAnimationsModule
 ];
 
 @NgModule({

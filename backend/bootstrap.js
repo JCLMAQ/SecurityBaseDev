19a9eb61af1c76149fcc5cdf973cf4fe1f377758
JCM <jcl.maquinay@gmail.com>
2019-04-17 17:14:21 +0200

    Add handler to avoir majus-minisc error

--- backend/bootstrap.js
+++ backend/bootstrap.js
@@ -1,1 +1,12 @@
-/* * A BootStrap is a JavaScript file that is run when your project is loaded on the server. * You can use it  to initialize your application, define HTTP pattern handlers etc.. *///directory.setSessionManager('login/session-manager');directory.setLoginManager("login/login-manager" , "Admin");httpServer.addRequestHandler('^/api/document/$', './handlers/handler.js', 'handleDocument');httpServer.addRequestHandler('^/api/document/[a-fA-F0-9]{32}/$', './handlers/handler.js', 'handleDocument');
+/*
+ * A BootStrap is a JavaScript file that is run when your project is loaded on the server.
+ * You can use it  to initialize your application, define HTTP pattern handlers etc..
+ */
+//directory.setSessionManager('login/session-manager');
+
+directory.setLoginManager("login/login-manager" , "Admin");
+
+httpServer.addRequestHandler('^/api/document/$', './handlers/handler.js', 'handleDocument');
+httpServer.addRequestHandler('^/api/document/[a-fA-F0-9]{32}/$', './handlers/handler.js', 'handleDocument');
+
+httpServer.addRequestHandler('(?i)/api/document/$', './handlers/handler.js', 'handleDocument');
